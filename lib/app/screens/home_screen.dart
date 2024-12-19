2be0d387621775e60b0b9c78eea3abb7a1a03389
Jesus Avelar <wisusdev@gmail.com>
2024-12-19 10:03:09 -0600

    Agregar pantalla principal con integración del servicio de temporizador y componentes de interfaz

---
+++ lib/app/screens/home_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:pomodoro/app/core/services/timer_service.dart';
+import 'package:pomodoro/app/widgets/progress.dart';
+import 'package:pomodoro/app/widgets/time_controller.dart';
+import 'package:pomodoro/app/widgets/timer_card.dart';
+import 'package:provider/provider.dart';
+
+class HomeScreen extends StatelessWidget {
+    const HomeScreen({super.key});
+    
+    Color renderColor(String currentState) {
+        if (currentState == "FOCUS") {
+            return Colors.black;
+        } else {
+            return Colors.green;
+        }
+    }
+
+    @override
+    Widget build(BuildContext context) {
+        final provider = Provider.of<TimerService>(context);
+        
+        return Scaffold(
+            backgroundColor: renderColor(provider.currentState),
+            
+            appBar: AppBar(
+                elevation: 10,
+                backgroundColor: renderColor(provider.currentState),
+                
+                title: const Text('POMOTIMER-*#!', style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700, color: Colors.yellow)),
+                
+                actions: [
+                    IconButton(
+                        icon: const Icon(Icons.refresh),
+                        iconSize: 40,
+                        color: Colors.white,
+                        onPressed: () => Provider.of<TimerService>(context, listen: false).reset(),
+                    ),
+                    IconButton(
+                        icon: const Icon(Icons.settings),
+                        iconSize: 40,
+                        color: Colors.white,
+                        onPressed: () => Navigator.pushNamed(context, '/settings'),
+                    ),
+                ],
+            ),
+            
+            body: SingleChildScrollView(
+                child: Container(
+                    alignment: Alignment.center,
+                    child: const Column(
+                        children: [
+                            SizedBox(height: 40),
+                            
+                            TimerCard(),
+                                                        
+                            SizedBox(height: 70),
+                            
+                            TimeController(),
+                            
+                            SizedBox(height: 70),
+
+                            Progress()
+                        ],
+                    ),
+                ),
+            ),
+        );
+    }
+}
